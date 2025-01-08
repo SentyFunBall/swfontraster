5f923774d6ec1c66578522e248134844d380fc88
SentyFunball <haydenbugatti@gmail.com>
2025-01-08 00:39:31 -0800

    user lerp instead for ease of reading

--- _build/libs/SenFont/utils.lua
+++ _build/libs/SenFont/utils.lua
@@ -16,9 +16,17 @@ SenFont.utils = {
         return point[1] * scale + offsetX, point[2] * scale + offsetY
     end;
 
-    evaluateQuadraticBezier = function(p0, p1, p2, t)
-        local x = (1 - t) * (1 - t) * p0[1] + 2 * (1 - t) * t * p1[1] + t * t * p2[1]
-        local y = (1 - t) * (1 - t) * p0[2] + 2 * (1 - t) * t * p1[2] + t * t * p2[2]
+    lerp = function(v0, v1, t)
+        return v0 + (v1 - v0) * t
+    end;
+
+    bezierInterpolation = function(p0, p1, p2, t) --slightly messy bc lerp doesnt use vectors
+        local x1 = SenFont.utils.lerp(p0[1], p1[1], t)
+        local y1 = SenFont.utils.lerp(p0[2], p1[2], t)
+        local x2 = SenFont.utils.lerp(p1[1], p2[1], t)
+        local y2 = SenFont.utils.lerp(p1[2], p2[2], t)
+        local x = SenFont.utils.lerp(x1, x2, t)
+        local y = SenFont.utils.lerp(y1, y2, t)
         return x, y
     end;
 }
